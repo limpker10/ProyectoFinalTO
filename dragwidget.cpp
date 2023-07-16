b333af29ebc6d08984efb8db48b70cc1d272e36c
Jose Cano <jcanov@unsa.edu.pe>
2023-07-16 18:20:51 -0500

    Implementando interfaces

--- dragwidget.cpp
+++ dragwidget.cpp
@@ -10,22 +10,35 @@ DragWidget::DragWidget(QWidget *parent)
     setAcceptDrops(true);
 
     QLabel *cuadrado = new QLabel(this);
-    cuadrado->setPixmap(QPixmap(":/images/cuadrado.png").scaled(cuadrado->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    cuadrado->move(10, 10);
+    cuadrado->setPixmap(QPixmap(":/images/cuadrado.png").scaled(QSize(100, 100), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    //cuadrado->move(10, 10);
     cuadrado->show();
     cuadrado->setAttribute(Qt::WA_DeleteOnClose);
+    cuadrado->setProperty("imageId", "cuadrado");
 
     QLabel *triangulo = new QLabel(this);
-    triangulo->setPixmap(QPixmap(":/images/triangulo.png").scaled(triangulo->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    triangulo->move(100, 10);
+    triangulo->setPixmap(QPixmap(":/images/triangulo.png").scaled(QSize(100, 100), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    //triangulo->move(100, 10);
     triangulo->show();
     triangulo->setAttribute(Qt::WA_DeleteOnClose);
+    triangulo->setProperty("imageId", "triangulo");
 
     QLabel *circulo = new QLabel(this);
-    circulo->setPixmap(QPixmap(":/images/circulo.png").scaled(circulo->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    circulo->move(10, 80);
+    circulo->setPixmap(QPixmap(":/images/circulo.png").scaled(QSize(100, 100), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    //circulo->move(10, 80);
     circulo->show();
     circulo->setAttribute(Qt::WA_DeleteOnClose);
+    circulo->setProperty("imageId", "circulo");
+
+    QLabel *box = new QLabel(this);
+    box->setPixmap(QPixmap(":/images/box.png").scaled(QSize(200, 170), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    int box1X = width() - box->width() - 10; // Posicionar a la derecha
+    int box1Y = 10; // Posicionar en la parte superior
+    box->move(box1X, box1Y);
+    box->show();
+    box->setAttribute(Qt::WA_DeleteOnClose);
+    box->setProperty("imageId", "box");
+
 }
 
 void DragWidget::dragEnterEvent(QDragEnterEvent *event)
@@ -71,7 +84,22 @@ void DragWidget::dropEvent(QDropEvent *event)
         newIcon->move(event->position().toPoint() - offset);
         newIcon->show();
         newIcon->setAttribute(Qt::WA_DeleteOnClose);
+        ////
 
+        QRect newIconRect = newIcon->geometry();
+
+        QList<QLabel *> childLabels = findChildren<QLabel *>();
+        auto isOverBox = std::any_of(childLabels.begin(), childLabels.end(), [&](QLabel *label) {
+            return label->property("imageId") == "box" && label->geometry().contains(newIcon->geometry());
+        });
+
+        if (isOverBox) {
+            qDebug() << "La nueva imagen está encima de un QLabel con el ID 'box'.";
+        } else {
+            qDebug() << "La nueva imagen no está encima de un QLabel con el ID 'box'.";
+        }
+
+        ///
         if (event->source() == this) {
             event->setDropAction(Qt::MoveAction);
             event->accept();
@@ -86,8 +114,9 @@ void DragWidget::dropEvent(QDropEvent *event)
 void DragWidget::mousePressEvent(QMouseEvent *event)
 {
     QLabel *child = static_cast<QLabel*>(childAt(event->position().toPoint()));
-    if (!child)
+    if (!child || child->property("imageId").toString() == "box") {
         return;
+    }
 
     QPixmap pixmap = child->pixmap();
 
--- game1.cpp
+++ game1.cpp
@@ -1,5 +1,4 @@
 #include "game1.h"
-#include "qdrag.h"
 #include "ui_game1.h"
 #include <QDragEnterEvent>
 #include <QDragMoveEvent>
@@ -18,11 +17,12 @@ Game1::Game1(QWidget *parent) :
     ui->setupUi(this);
 
     // Habilitar la funcionalidad de arrastrar y soltar en el marco
-    ui->frame->setAcceptDrops(true);
-    QHBoxLayout *horizontalLayout = new QHBoxLayout;
-    horizontalLayout->addWidget(new DragWidget);
-    horizontalLayout->addWidget(new DragWidget);
-    this->setLayout(horizontalLayout);
+
+    //QHBoxLayout *horizontalLayout = new QHBoxLayout;
+    //horizontalLayout->addWidget(new DragWidget);
+    ui->horizontalLayout->addWidget(new DragWidget);
+    //horizontalLayout->addWidget(new DragWidget);
+    //this->setLayout(horizontalLayout);
 }
 
 Game1::~Game1()
@@ -53,101 +53,25 @@ void Game1::end(QMainWindow* mainWindow)
 
 void Game1::setupImages()
 {
-    // Configurar la imagen para la etiqueta label
-    QPixmap cuadrado(":/images/cuadrado.png"); // Cargar el recurso "cuadrado.png" desde el archivo de recursos
 
-    ui->label->setPixmap(cuadrado);
-    ui->label->setScaledContents(true);
-    ui->label->setAttribute(Qt::WA_DeleteOnClose);
 }
 
 void Game1::dragEnterEvent(QDragEnterEvent *event)
 {
-    if (event->mimeData()->hasFormat("application/x-dnditemdata")) {
-        if (event->source() == this) {
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-        } else {
-            event->acceptProposedAction();
-        }
-    } else {
-        event->ignore();
-    }
+
 }
 
 void Game1::dragMoveEvent(QDragMoveEvent *event)
 {
-    if (event->mimeData()->hasFormat("application/x-dnditemdata")) {
-        if (event->source() == this) {
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-        } else {
-            event->acceptProposedAction();
-        }
-    } else {
-        event->ignore();
-    }
+
 }
 
 void Game1::dropEvent(QDropEvent *event)
 {
-    if (event->mimeData()->hasFormat("application/x-dnditemdata")) {
-        QByteArray itemData = event->mimeData()->data("application/x-dnditemdata");
-        QDataStream dataStream(&itemData, QIODevice::ReadOnly);
 
-        QPixmap pixmap;
-        QPoint offset;
-        dataStream >> pixmap >> offset;
-
-        QLabel *newIcon = new QLabel(ui->frame);
-        newIcon->setPixmap(pixmap);
-        newIcon->move(event->position().toPoint() - offset);
-        newIcon->show();
-        newIcon->setAttribute(Qt::WA_DeleteOnClose);
-
-        if (event->source() == this) {
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-            // Verificar si la imagen se movió al marco
-            if (ui->frame->geometry().contains(event->pos())) {
-                qDebug() << "La imagen se movió al marco.";
-            }
-        } else {
-            event->acceptProposedAction();
-        }
-    } else {
-        event->ignore();
-    }
 }
 
 void Game1::mousePressEvent(QMouseEvent *event)
 {
-    QLabel *child = static_cast<QLabel*>(childAt(event->position().toPoint()));
-    if (!child)
-        return;
 
-    QPixmap pixmap = child->pixmap();
-
-    QByteArray itemData;
-    QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-    dataStream << pixmap << QPoint(event->position().toPoint() - child->pos());
-
-    QMimeData *mimeData = new QMimeData;
-    mimeData->setData("application/x-dnditemdata", itemData);
-
-    QDrag *drag = new QDrag(this);
-    drag->setMimeData(mimeData);
-    drag->setPixmap(pixmap);
-    drag->setHotSpot(event->position().toPoint() - child->pos());
-
-    QPixmap tempPixmap = pixmap;
-
-    child->setPixmap(tempPixmap);
-
-    if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction) {
-        child->close();
-    } else {
-        child->show();
-        child->setPixmap(pixmap);
-    }
 }
